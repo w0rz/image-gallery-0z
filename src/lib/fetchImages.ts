5021bf6e57048d1e0d550eaa82bf36ed9f4bed6a
Ig0r <w0rz@protonmail.com>
2024-03-04 20:59:47 -0300

    fetchImages

---
+++ src/lib/fetchImages.ts
@@ -0,0 +1,24 @@
+import type { ImagesResults } from "@/models/Images";
+import { ImagesSchemaWithPhotos } from "@/models/Images";
+import env from "./env";
+
+export default async function fetchImages(
+  url: string,
+): Promise<ImagesResults | undefined> {
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: env.PEXELS_API_KEY,
+      },
+    });
+    if (!res.ok) throw new Error("Fetch Images error!\n");
+    const imagesResults: ImagesResults = await res.json();
+    // Parse data with Zod schema
+    const parsedData = ImagesSchemaWithPhotos.parse(imagesResults);
+    if (parsedData.total_results === 0) return undefined;
+    return parsedData;
+  } catch (error) {
+    // Will show in terminal console
+    if (error instanceof Error) console.log(error.stack);
+  }
+}
