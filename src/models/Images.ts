971cf8023b5d62584728288c34bb12179c099da4
Ig0r <w0rz@protonmail.com>
2024-03-04 21:03:57 -0300

    Zod

--- src/models/Images.ts
+++ src/models/Images.ts
@@ -1,16 +1,15 @@
 import { z } from "zod";
 
-export type Photo = z.infer<typeof PhotoSchema>
-export type ImagesResults = z.infer<typeof ImagesSChemaWithPhotos>
-
+export type Photo = z.infer<typeof PhotoSchema>;
+export type ImagesResults = z.infer<typeof ImagesSchemaWithPhotos>;
 
 const BasicImageSchema = z.object({
   page: z.number(),
-per_page: z.number(),
-prev_page: z.string().optional(),
+  per_page: z.number(),
+  prev_page: z.string().optional(),
   next_page: z.string().optional(),
   total_results: z.number(),
-})
+});
 
 const PhotoSchema = z.object({
   id: z.number(),
@@ -22,13 +21,8 @@ const PhotoSchema = z.object({
   }),
   alt: z.string(),
   blurredDataUrl: z.string().optional(),
-})
-{
-  "per_page": 1,
-  "next_page": "https://api.pexels.com/v1/curated/?page=2&per_page=1"
-}
+});
 
-export const ImagesSChemaWithPhotos = BasicImageSchema.extend({
-  photos: z.array(PhotoSchema)
-})
-
+export const ImagesSchemaWithPhotos = BasicImageSchema.extend({
+  photos: z.array(PhotoSchema),
+});
