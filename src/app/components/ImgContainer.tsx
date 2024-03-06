fe6f27b8e7c1f94e64b67366cbb13f6247247bd0
Ig0r <w0rz@protonmail.com>
2024-03-05 22:43:33 -0300

    Optimized responsiveness of images

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,8 +7,17 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
   return (
-    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
-      <Image src={photo.src.large} alt={photo.alt} width={250} height={250} />
+    <div
+      key={photo.id}
+      className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
+    >
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        sizes="(min-width: 1620px) 276px, (min-width: 1320px) calc(8.57vw + 139px), (min-width: 1060px) 25vw, (min-width: 800px) calc(33.33vw - 14px), (min-width: 540px) calc(50vw - 15px), calc(100vw - 20px)"
+        className="object-cover group-hover:opacity-75"
+      />
     </div>
   );
 }
