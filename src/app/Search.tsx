7a9248afed1750bc762d423dbc78c9b041aeb842
Ig0r <w0rz@protonmail.com>
2024-03-08 02:23:31 -0300

    Resolved issue: Preventing 404 error upon hitting 'Enter' in the search bar

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -8,7 +8,7 @@ export default function Search() {
   const router = useRouter();
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    router.push(`/results/${search}`);
+    if (search) router.push(`/results/${search}`);
     setSearch("");
   };
   return (
