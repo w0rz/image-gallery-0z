55b36855d785ccc8c287f3baeb509a53bf5748ef
Ig0r <w0rz@protonmail.com>
2024-03-08 00:17:15 -0300

    Pexels search query & Tailwind refactoring

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -3,15 +3,21 @@ import type { ImagesResults } from "@/models/Images";
 import ImgContainer from "./ImgContainer";
 import addBlurredDataUrls from "@/lib/getBase64";
 
-export default async function Gallery() {
-  const url = "https://api.pexels.com/v1/curated";
+type Props = {
+  term?: string | undefined;
+};
+
+export default async function Gallery({ term }: Props) {
+  const url = !term
+    ? "https://api.pexels.com/v1/curated"
+    : `https://api.pexels.com/v1/search?query=${term}`;
   const images: ImagesResults | undefined = await fetchImages(url);
 
   if (!images) return <h2 className="m-4 text-2xl">No Images Found</h2>;
 
   const photosWithBlur = await addBlurredDataUrls(images);
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+    <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
       {photosWithBlur.map((photo) => (
         <ImgContainer key={photo.id} photo={photo} />
       ))}
